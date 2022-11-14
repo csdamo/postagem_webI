84d42bb0e015c42f6e037c1fa6d4d19429eba846
csdamo <cristiane@classcode.com.br>
2022-11-14 00:30:05 -0300

    Cria função para metodo post da api e modal com formulário

--- js/script.js
+++ js/script.js
@@ -12,10 +12,59 @@ window.onload = function(){
   console.log("Busca 5 primeiras postages");
   obterPostagens(-1, "", "GET", 0, 5);
 
-  console.log("Insere postagem");
-  obterPostagens(-1, "", "POST", 0, 0, {titulo: "Teste", conteudo: "Teste", categorias: "Teste"});
+  // console.log("Insere postagem");
+  // obterPostagens(-1, "", "POST", 0, 0, {titulo: "Teste", conteudo: "Teste", categorias: "Teste"});
 
 }
+
+// método para chamar rota api com ajax
+function runAjax(post_url, request_method, form_data){
+  $.ajax({
+		url : post_url,
+		type: request_method,
+		data : form_data,
+    dataType: 'json',
+    contentType: "application/json"
+	})
+  .done(function() {
+    console.log("success");
+  })
+  .fail(function(erro) {
+    console.log("erro", erro);
+  })
+  .always(function() {
+    console.log("complete");
+  });
+}
+
+// Quando form é enviado, identifica chama a rota de post ou put
+$("#form_id").submit(function(event){
+	event.preventDefault(); //prevent default action 
+  
+  let id = $("#item_id").val();
+  let title = $("#title_id").val();
+  let categories = $("#categories_id").val();
+  let content = $("#content_id").val();
+
+  let post_url = 'https://localhost:4567/postagem';
+  let request_method = "POST";
+  
+  if(id){
+    request_method = "PUT";
+    post_url = post_url + "/" + id;
+  }
+
+  let body = {
+    "title": title,
+    "categories": [categories],
+    "content": content
+  }
+
+  let form_data = JSON.stringify(body);
+
+  runAjax(post_url, request_method, form_data)
+
+});
 
 function obterPostagens(postagem = -1, titulo = "", metodo="GET", inicio=0, limite=0, data){
 
